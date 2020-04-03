4dba4db8dc8f82484708d5e1c54e55e9ffa497f5
Amin <mahjoub@usc.edu>
2020-04-03 16:48:22 -0700

    a

--- lane_detect.py
+++ lane_detect.py
@@ -6,13 +6,15 @@ cap = cv2.VideoCapture(0)
 while(True):
 	ret, frame = cap.read()
 
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	
 
 	cv2.imshow('frame', hsv)
 	lower_blue = np.array([60,40,40])
 	upper_blue = np.array([150, 255, 255])
 	mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
