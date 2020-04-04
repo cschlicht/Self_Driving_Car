0871398e2c78e086c3315cc800e3af986833a0d1
Amin <mahjoub@usc.edu>
2020-04-03 17:22:18 -0700

    a

--- lane_detect.py
+++ lane_detect.py
@@ -1,11 +1,27 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
 
-while(True):
+
+
+    
+
+       
+def main():
+    while (True):
+        cap = cv2.VideoCapture(0)
+        Detect_Edges()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+def Detect_Edges():
+
     ret, frame = cap.read()
-
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     
@@ -13,16 +29,18 @@ while(True):
     upper_blue = np.array([150, 255, 255])
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     
-
-    cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
-    
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    edges = cv2.Canny(mask, 200, 400)
 
-cap.release()
-cv2.destroyAllWindows()
+    return edges
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 
     
