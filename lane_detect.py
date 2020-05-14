d1e0925c36afc6069803bd4c6d10e94afc96acfa
Amin Mahjoub <mahjoub@usc.edu>
2020-05-14 11:10:54 -0700

    a

--- lane_detect.py
+++ lane_detect.py
@@ -56,7 +56,7 @@ def Cut_top_half(edges):
 def Detect_line_segment(cropped_edges):
     rho = 1 #distance precision in pixel
     angle = np.pi / 180 
-    min_threshold = 100 
+    min_threshold = 80 
     min_line_length = 30
     max_line_gap = 10
     line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, np.array([]), min_line_length, max_line_gap)
