6cf3ee739d7a74266dfce4b482088cbb3c6a08aa
Amin Mahjoub <mahjoub@usc.edu>
2020-05-14 17:32:55 -0700

    a

--- lane_detect.py
+++ lane_detect.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import warnings 
 import time
 import math
-import motor_main as motor
-import car_dir as car
-import RPi.GPIO as GPIO
+#import motor_main as motor
+#import car_dir as car
+#import RPi.GPIO as GPIO
 #import PCA9685 as p
 #import Sunfounder_Smart_Video_Car_Kit_for_RaspberryPi
 
